a80732349c31335bef00b2b29af69534d3df3d17
decarvalhobo <marcio.decarvalho@student.uclouvain.be>
2016-03-15 13:27:30 +0100

    questions

--- Denauw_DeCarvalho/CodeSource/code.c
+++ Denauw_DeCarvalho/CodeSource/code.c
@@ -19,10 +19,15 @@ void* my_malloc(int size)
 {
 	/*Initialisation*/
     if (debut_workspace_m==NULL) 	//si c'est le premier appel de my_malloc, on crée un workspace
-    {	
+    {
         debut_workspace_m = sbrk(size);	//on stocke l'adresse du début du workspace en l'incrémentant de 'size'
         fin_workspace_m=sbrk(0);		//on stocke l'adresse de fin du workspace
     }
+    void *position_debut_m = debut_workspace_m; //curseur début pour la recherche
+	void *position_fin_m = debut_workspace_m; //curseur fin pour la recherche
+
+	/*Questions: j'ai une adresse, comment savoir si il y a un block?
+			j'ai une adresse comment mettre un block?*/
 
     /*Allocation d'un bloc entre 'debut_workspace_m' et 'fin_workspace_m'*/
 
@@ -34,20 +39,3 @@ void myfree(void *ptr)
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-int main(int argc, char const *argv[])
-{
-	
-	return 0;
-}
