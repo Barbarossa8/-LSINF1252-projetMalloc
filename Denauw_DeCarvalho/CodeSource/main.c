560080ea94a9ef3593ca8575de5a20efeb77df2b
decarvalhobo <marcio.decarvalho@student.uclouvain.be>
2016-03-16 18:13:29 +0100

    free ok

--- Denauw_DeCarvalho/CodeSource/main.c
+++ Denauw_DeCarvalho/CodeSource/main.c
@@ -20,35 +20,53 @@ int main(int argc, char *argv[])
 	
 	//mini tests
 	void* a=my_malloc(8);
-	void* b=a;
+	void* b=my_malloc(10);
+	void* c=my_malloc(4);
+	
 	printf("\nadresse malloc(8): %p \n",a);
-	
 	a-=4;
-	printf("adresse malloc(8)-4: %p \n",a);
-	struct block_header *header2=(struct block_header*) a;
-	printf("alloué: %d \n",header2->alloc);
-	printf("size: %d \n",header2->size);
-	a+=header2->size;
-	struct block_header *header3=(struct block_header*) a;
-	printf("\nadresse bloc suivant après 12: %p \n",a);
-	printf("alloué: %d \n",header3->alloc);
-	printf("size: %d \n",header3->size);
-	a-=header2->size;
+	struct block_header *headera=(struct block_header*) a;
+	printf("alloué: %d \n",headera->alloc);
+	printf("size: %d \n",headera->size);
 	
-	printf("\npointeur debut block_header: %p \n",a);
-	a+=4;
-	my_free(a);	
-	a-=4;
-	struct block_header *header4=(struct block_header*) a;
-	//header4->alloc=0;
-	printf("alloué: %d \n",header4->alloc);
-	printf("size: %d \n",header4->size);
-	//free(a);
-	/*b-=4;
-	struct block_header *header4=(struct block_header*) b;
-	printf("\nadresse bloc suivant après free: %p \n",b);
-	printf("alloué: %d \n",header4->alloc);
-	printf("size: %d \n",header4->size);*/
+	printf("\nadresse malloc(10): %p \n",b);
+	b-=4;
+	struct block_header *headerb=(struct block_header*) b;
+	printf("alloué: %d \n",headerb->alloc);
+	printf("size: %d \n",headerb->size);
+
+	printf("\nadresse malloc(4): %p \n",c);
+	c-=4;
+	struct block_header *headerc=(struct block_header*) c;
+	printf("alloué: %d \n",headerc->alloc);
+	printf("size: %d \n",headerc->size);
+	c+=headerc->size;
+	printf("\nadresse fin malloc(4): %p ",c);
+	printf("\nadresse fin : %p \n",fin_workspace_m);
+
+	my_free(a+4);
+
+	//Après le free de a
+	printf("\nadresse malloc(8): %p \n",a+4);
+	//a-=4;
+	headera=(struct block_header*) a;
+	printf("alloué: %d \n",headera->alloc);
+	printf("size: %d \n",headera->size);
+	
+	my_free(b+4);
+	//Après le free de b
+	printf("\nadresse malloc(10): %p \n",b+4);
+	//b-=4;
+	headerb=(struct block_header*) b;
+	printf("alloué: %d \n",headerb->alloc);
+	printf("size: %d \n",headerb->size);
+
+	//Ici on a donc a qui s'est mis avec b 
+	printf("\nadresse malloc(8): %p \n",a+4);
+	//a-=4;
+	headera=(struct block_header*) a;
+	printf("alloué: %d \n",headera->alloc);
+	printf("size: %d \n",headera->size);
 	return 0;
 }
 
